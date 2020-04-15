cc9b52503308f41466fe28b5e9bb1bd23b2aa0e0
Kate Cook <kate.cook@gmail.com>
2020-04-15 10:57:45 -0400

    add download script

---
+++ download_fastq.sh
@@ -0,0 +1,7 @@
+output=/home/katecook/Documents/HC_EHSRB/projects/ddesaulniers_methylation/fastq
+
+fastq-dump --outdir $output --gzip --skip-technical  --readids --read-filter pass --dumpbase --clip SRR8234804
+fastq-dump --outdir $output --gzip --skip-technical  --readids --read-filter pass --dumpbase --clip SRR8234805
+fastq-dump --outdir $output --gzip --skip-technical  --readids --read-filter pass --dumpbase --clip SRR8234806
+fastq-dump --outdir $output --gzip --skip-technical  --readids --read-filter pass --dumpbase --clip SRR8234802
+fastq-dump --outdir $output --gzip --skip-technical  --readids --read-filter pass --dumpbase --clip SRR8234803
